5f36caeae01dedf700743463a775302ca25e88f1
Neagoe Ciprian <neagoeciprian16@yahoo.com>
2018-11-15 16:03:19 +0200

    Added player basic login system

--- BasicGamemode/World/Player.cs
+++ BasicGamemode/World/Player.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Linq;
 using GamemodeDatabase;
-using SampSharp.GameMode.Pools;
-using SampSharp.GameMode.World;
-using SampSharp.GameMode.SAMP;
 using GamemodeDatabase.Models;
-using Microsoft.EntityFrameworkCore;
+using SampSharp.GameMode;
 using SampSharp.GameMode.Definitions;
 using SampSharp.GameMode.Display;
+using SampSharp.GameMode.Pools;
+using SampSharp.GameMode.SAMP;
+using SampSharp.GameMode.World;
 
 namespace BasicGamemode.World
 {
@@ -20,7 +20,7 @@ namespace BasicGamemode.World
             {
                 using (var db = new GamemodeContext())
                 {
-                    return db.Players.Where(x => x.PlayerName == this.Name).SingleOrDefault();
+                    return db.Players.Where(x => x.PlayerName == Name).SingleOrDefault();
                 }
             }
         }
@@ -30,50 +30,75 @@ namespace BasicGamemode.World
         {
             GameText("Test RPG", 2000, 5);
 
-            SendClientMessage(Color.GreenYellow, $"Welcome to the server {this.Name}! Have fun!");
+            SendClientMessage(Color.GreenYellow, $"Welcome to the server {Name}! Have fun!");
 
             SetWorldBounds(2500.0f, 1850.0f, 631.2963f, -454.9898f);
 
-            try
+
+            if (Account is null)
             {
-                if (Account is null)
+                var dialog = new InputDialog("Register", "Input your password", true, "Register", "Cancel");
+                dialog.Show(this);
+                dialog.Response += (sender, ev) =>
                 {
-                    var dialog = new InputDialog("Register", "Input your password", true, "Accept", "Cancel");
-                    dialog.Show(this);
-                    dialog.Response += (sender, ev) =>
+                    switch (ev.DialogButton)
                     {
-                        switch (ev.DialogButton)
+                        case DialogButton.Left:
                         {
-                            case DialogButton.Left:
+                            using (var db = new GamemodeContext())
                             {
-                                using (var db = new GamemodeContext())
+                                var player = new PlayerModel
                                 {
-                                    var player = new PlayerModel
-                                    {
-                                        Password = ev.InputText,
-                                        PlayerName = this.Name
-                                    };
-                                    db.Players.Add(player);
-                                    db.SaveChanges();
+                                    Password = ev.InputText,
+                                    PlayerName = Name
+                                };
+                                db.Players.Add(player);
+                                db.SaveChanges();
+                            }
+                        }
+                            break;
+                        case DialogButton.Right:
+                        {
+                            Kick();
+                        }
+                            break;
+                    }
+                };
+            }
+            else
+            {
+                var dialog = new InputDialog("Login", "Insert your password", true, "Login", "Cancel");
+                dialog.Show(this);
+                dialog.Response += (sender, ev) =>
+                {
+                    switch (ev.DialogButton)
+                    {
+                        case DialogButton.Left:
+                        {
+                            using (var db = new GamemodeContext())
+                            {
+                                var player = Account;
+                                if (ev.InputText == player.Password)
+                                {
+                                    SetSpawnInfo(NoTeam, 0, new Vector3(1.0, 1.0, 1.5), 90.0f);
+                                    Spawn();
+                                }
+                                else
+                                {
+                                    SendClientMessage(Color.Red, "Wrong password");
                                 }
                             }
-                                break;
-                            case DialogButton.Right:
-                            {
-                                this.Kick();
-                            }
-                                break;
                         }
-                    };
+                            break;
+                        case DialogButton.Right:
+                        {
+                            Kick();
+                        }
+                            break;
+                    }
+                };
+            }
 
-                }
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine(exception);
-                throw;
-            }
-            
 
             base.OnConnected(e);
         }
