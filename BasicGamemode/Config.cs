d8a1d4821fe7b70ea5793fdb84c8712a482b6bc3
Neagoe Ciprian <neagoeciprian16@yahoo.com>
2018-11-16 11:17:17 +0200

    Added documentation to the config class

--- BasicGamemode/Config.cs
+++ BasicGamemode/Config.cs
@@ -1,7 +1,13 @@
 ﻿namespace BasicGamemode
 {
+    /// <summary>
+    /// Static class containing all the static config values
+    /// </summary>
     public static class Config
     {
+        /// <summary>
+        /// Holds the number of MaximumLoginTries
+        /// </summary>
         public static int MaximumLoginTries = 3;
     }
 }
