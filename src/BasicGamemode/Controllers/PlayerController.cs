59795e7f379161d70a54c563b3069b6c09601d57
Neagoe Ciprian <neagoeciprian16@yahoo.com>
2018-11-18 15:09:35 +0200

    Added documentation

--- src/BasicGamemode/Controllers/PlayerController.cs
+++ src/BasicGamemode/Controllers/PlayerController.cs
@@ -3,8 +3,14 @@ using SampSharp.GameMode.Controllers;
 
 namespace BasicGamemode.Controllers
 {
+    /// <summary>
+    ///     Share BasePlayer events with BasePlayer child
+    /// </summary>
     public class PlayerController : BasePlayerController
     {
+        /// <summary>
+        ///     Register the BasePlayer childs types
+        /// </summary>
         public override void RegisterTypes()
         {
             Player.Register<Player>();
